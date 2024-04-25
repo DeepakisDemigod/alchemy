5baee94b3a56a90480e46e2495c10d3611d6bceb
DeepakisDemigod <deepakthapa1423@gmail.com>
2024-04-25 19:43:31 +0530

    routes

---
+++ dist/assets/About-BCh7vmF2.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BxiVbxov.js";const r=()=>t.jsx("div",{children:"about page"});export{r as default};
---
+++ dist/assets/Contact-O64xgHim.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BxiVbxov.js";const r=()=>t.jsx("div",{children:"contact page"});export{r as default};
