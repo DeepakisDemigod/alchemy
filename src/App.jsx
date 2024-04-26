68450aed93b68316c31fdf600fac0d2b07077c14
DeepakisDemigod <deepakthapa1423@gmail.com>
2024-04-26 16:50:42 +0530

    built

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
-import React from 'react';
-const About = React.lazy(() => import('./components/About.jsx'));
-const Contact = React.lazy(() => import('./components/Contact.jsx'));
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <BrowserRouter basename="/">
-        <div>
-          <a href='/about'>about</a>
-          <a href='/contact'>contact</a>
-          <Routes>
-            <Route
-              path='/about'
-              element={<About />}
-            />
-            <Route
-              path='/contact'
-              element={<Contact />}
-            />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default App;
+        {/* A <Routes> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
--- src/dist/assets/Contact-T5sW0iDH.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Bz8UiVsy.js";const r=()=>t.jsx("div",{children:"contact page"});export{r as default};
