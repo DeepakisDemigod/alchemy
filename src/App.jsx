55f1683d33b735f18d59d31ac8ce77fe41a79f47
DeepakisDemigod <deepakthapa1423@gmail.com>
2024-04-27 13:12:12 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-import React from "react";
-import About from './About.jsx'
-import Contact from './Contact.jsx'
-import {
-  HashRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React from 'react';
+import About from './About.jsx';
+import Home from './Home.jsx';
+import Contact from './Contact.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <nav>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <a href='/'>Home</a>
+        <a href='/about'>About</a>
+        <a href='/contact'>Contact</a>
       </nav>
       <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/about'
+          element={<About />}
+        />
+        <Route
+          path='/contact'
+          element={<Contact />}
+        />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
